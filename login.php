82905d5fdf6356ddb7b22c469e5ca9c25110562f
lubarog13 <lubarog13@gmail.com>
2021-05-27 20:46:05 +0300

    Init commit

---
+++ login.php
@@ -0,0 +1,25 @@
+<form method="POST">
+Email: <input type="text" name="login"><br>
+Пароль: <input type="text" name="password"><br>
+<button type="submit" name="enter">Войти</button>
+<a href="registration.php">Регистрация</a>
+</form>
+
+<?php
+    if(isset($_POST['enter'])){
+    session_start();
+    $mysqli = new mysqli("localhost", "root", "1234", "rpm_project");
+        if(isset($_POST['login'], $_POST['password'])){
+            $str = "SELECT * FROM users where login = '".$_POST['login']."' and password='".$_POST['password']."'";
+            $result = $mysqli->query($str);
+            $row = $result->fetch_assoc();
+            if($row != null){
+                $_SESSION['login'] = $_POST['login'];
+                $_SESSION['password'] = $_POST['password'];
+            }
+            else echo "Проверьте email и пароль или зарегестрируйтесь";
+        }
+        else echo "Введите все поля";
+    $mysqli->close();
+    }
+?>
---
+++ registration.php
@@ -0,0 +1,29 @@
+<form method="POST">
+Email: <input type="text" name="login"><br>
+Имя: <input type="text", name="first_name"><br>
+Пароль: <input type="text" name="password"><br>
+Повторите пароль: <input type="text" name="password_d"><br>
+<button type="submit">Зарегестрироваться</button>
+<a href="login.php">Войти</a>
+</form>
+<?php
+    if(isset($_POST['login'], $_POST['password'], $_POST['password_d'], $_POST['first_name'])){
+        if($_POST['password']==$_POST['password_d']){
+            $mysqli = new mysqli("localhost", "root", "1234", "rpm_project");
+            $str = "SELECT * FROM users where login = '".$_POST['login']."'";
+            $result = $mysqli->query($str);
+            if($row = $result->fetch_assoc()) echo "Запись уже существует";
+            else{
+            $str = "INSERT INTO users(login, name, password) VALUES ("."'".$_POST['login']."'".", '".$_POST['first_name']."', '".$_POST['password']."')";
+            session_start();
+            $mysqli->query($str);
+            $_SESSION['login']=$_POST['login'];
+            $_SESSION['first_name'] = $_POST['first_name'];
+            $_SESSION['password'] = $_POST['password'];
+            }
+            $mysqli->close();
+        }
+        else echo "Пароли не совпадают";
+    }
+    else echo "Введите все поля";
+?>
